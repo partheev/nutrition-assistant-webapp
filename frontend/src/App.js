760fce1df95f7a11f766c36398d292e5d6f0bb7d
poornesh <poorneshchenna29@gmail.com>
2022-09-15 20:30:06 +0530

    home and signup page designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/auth/Signup'
+import Signin from './pages/auth/Signin'
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Home />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
